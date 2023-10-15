d6ad47e972def3cb860382110ab4a808d7338585
laoer536 <1547698569@qq.com>
2023-10-15 17:51:47 +0800

    feat: prisma cli and api auth demo

--- src/app/api/user/users/route.ts
+++ src/app/api/user/users/route.ts
@@ -1,6 +1,13 @@
 import { prisma } from '@/collection/mysql'
+import { getServerSession } from 'next-auth/next'
+import { authOptions } from '@/lib/auth'
 
 export async function GET(request: Request) {
   const users = await prisma.user.findMany()
-  return Response.json(users)
+  const session = await getServerSession(authOptions)
+  if (session) {
+    return Response.json(users)
+  } else {
+    return Response.json({ message: 'Not logged in.' }, { status: 401 })
+  }
 }
