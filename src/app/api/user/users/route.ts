0fc2947510379c9af259e4ac4fea00878e8f979d
laoer536 <1547698569@qq.com>
2023-10-15 17:53:51 +0800

    perf: api auth demo

--- src/app/api/user/users/route.ts
+++ src/app/api/user/users/route.ts
@@ -3,9 +3,9 @@ import { getServerSession } from 'next-auth/next'
 import { authOptions } from '@/lib/auth'
 
 export async function GET(request: Request) {
-  const users = await prisma.user.findMany()
   const session = await getServerSession(authOptions)
   if (session) {
+    const users = await prisma.user.findMany()
     return Response.json(users)
   } else {
     return Response.json({ message: 'Not logged in.' }, { status: 401 })
