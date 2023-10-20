1f1b50d0b8ca31ab12b2850621ba7b8678853827
laoer536 <1547698569@qq.com>
2023-10-20 23:55:54 +0800

    perf: useFetch methods type add

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,8 @@
+import { HTTP_METHOD } from 'next/dist/server/web/http'
+
 export async function useFetch<T, P = unknown>(
   input: NodeJS.fetch.RequestInfo,
-  init?: RequestInit & { body: P },
+  init?: RequestInit & { body?: P; method?: HTTP_METHOD },
 ): Promise<T> {
   if (typeof input === 'string' && !input.startsWith('http')) {
     const origin = process.env.APP_ORIGIN as string
