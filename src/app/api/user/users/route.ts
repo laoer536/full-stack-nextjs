22d23daf3cddecb6486b9828c605a62fdffc7d36
LiuJie <1547698569@qq.com>
2024-07-07 17:41:11 +0800

    feat: not login page

--- src/app/api/user/users/route.ts
+++ src/app/api/user/users/route.ts
@@ -1,13 +1,5 @@
 import { prisma } from '@/lib/collection/mysql'
-import { getServerSession } from 'next-auth/next'
-import { authOptions } from '@/lib/auth'
-
 export async function GET(request: Request) {
-  const session = await getServerSession(authOptions)
-  if (session) {
-    const users = await prisma.user.findMany()
-    return Response.json(users)
-  } else {
-    return Response.json({ message: 'Not logged in.' }, { status: 401 })
-  }
+  const users = await prisma.user.findMany()
+  return Response.json(users)
 }
