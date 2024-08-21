b3fc6c81815c26fb5b670451d13ebec91ea03e76
LiuJie <1547698569@qq.com>
2024-08-22 00:38:56 +0800

    perf: add user

--- src/lib/collection/mysql.ts
+++ src/lib/collection/mysql.ts
@@ -1,13 +1,4 @@
 /** Use prisma ORM  **/
 import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
-prisma.$use(async (params, next) => {
-  const now = new Date()
-  console.log({
-    time: `${now.toLocaleDateString()}-${now.toLocaleTimeString()}`,
-    [`${params.model}.${params.action}`]: params.args,
-  })
-  return next(params)
-})
-
 export { prisma }
