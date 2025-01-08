0751e242e51b0df5bbc0b5ffee351ec61236f707
LiuJie <1547698569@qq.com>
2025-01-09 02:31:16 +0800

    style: dockerfile

--- prisma/migrations/20231014094052_auth_add/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE `verificationtokens` (
-    `identifier` VARCHAR(191) NOT NULL,
-    `token` VARCHAR(191) NOT NULL,
-    `expires` DATETIME(3) NOT NULL,
-
-    UNIQUE INDEX `verificationtokens_token_key`(`token`),
-    UNIQUE INDEX `verificationtokens_identifier_token_key`(`identifier`, `token`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
--- src/lib/collection/mysql.ts
+++
@@ -1,4 +0,0 @@
-/** Use prisma ORM  **/
-import { PrismaClient } from '@prisma/client'
-const prisma = new PrismaClient()
-export { prisma }
